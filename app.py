c46c23f01d631a088b4c50f8ccabd66f8cd49841
Karina Mashanova <karmen.01@inbox.ru>
2022-03-22 00:55:57 +0300

    Test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import speech_recognition
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from forms import DataForm
 
 app = Flask(__name__)
@@ -10,21 +10,16 @@ app.config['SECRET_KEY'] = "dick"
 def index():  # put application's code here
     return render_template("index.html")
 
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
-@app.route("/DataForm")
-def DataForm():
-    template = "DataForm.html"
-    return render_template(template)
 
-@app.route("/get-voice")
-def get_voice():
-    template= "DataForm.html"
-    recognizer = speech_recognition.Recognizer()
-    with speech_recognition.Microphone() as source:
-        audio = recognizer.listen(source)
-    userRequest = recognizer.recognize_google(audio)
 
-    return render_template(template, response=userRequest)
+
